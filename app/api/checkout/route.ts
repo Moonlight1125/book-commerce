fcd5d503c0defbbcdf67adef9e431f5a010d20ea
kentaro <k.suzuki1125@outlook.jp>
2025-03-20 12:13:48 +0900

    apikeyを提供

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY!);
 
 export const POST = async (req:Request) => {
     const { title, price ,bookId,userId} = await req.json();
