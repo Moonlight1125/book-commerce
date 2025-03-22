014372d831d86c68a0328654d1d78d5de7b89528
kentaro <k.suzuki1125@outlook.jp>
2025-03-22 22:59:11 +0900

    環境変数の追加

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+const stripe = new Stripe('sk_test_51R3SxbLUnc6ePoMLdmqN9tUGOwbs92t4FpX3FCtTqvHSV3kOYtKkCjacbkfKFApHT1uhTB0ErekuHxE3hF90LTvE00M2hwpR8u');
 
 export const POST = async (req:Request) => {
     const { title, price ,bookId,userId} = await req.json();
@@ -28,8 +28,8 @@ export const POST = async (req:Request) => {
                 },
             ],
             mode:'payment',
-            success_url:`${process.env.NEXT_PUBLIC_HOME_URL}/book/checkout-success?session_id={CHECKOUT_SESSION_ID}`,
-            cancel_url:`${process.env.NEXT_PUBLIC_HOME_URL}}`,
+            success_url:`https://book-commerce-sandy.vercel.app/book/checkout-success?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url:`https://book-commerce-sandy.vercel.app`,
         });
         return NextResponse.json({checkout_url:session.url});
     } catch(err:any){
